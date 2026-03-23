105246268726515021a22142391a7ec42d93e6fe
erinda7I <erinda.ismani@ibu.edu.mk>
2026-03-22 20:45:04 -0400

    Complete week 1 assignments: hello, mario, cash, credit

---
+++ pset1/mario/mario_more.c
@@ -0,0 +1,34 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int height;
+    do
+    {
+        height = get_int("Height: ");
+    }
+    while (height < 1 || height > 8);
+    
+    for (int i = 0; i < height; i++)
+    {
+        // Left spaces
+        for (int j = 0; j < height - i - 1; j++)
+        {
+            printf(" ");
+        }
+        // Left hashes
+        for (int j = 0; j <= i; j++)
+        {
+            printf("#");
+        }
+        // Two spaces in middle
+        printf("  ");
+        // Right hashes
+        for (int j = 0; j <= i; j++)
+        {
+            printf("#");
+        }
+        printf("\n");
+    }
+}
